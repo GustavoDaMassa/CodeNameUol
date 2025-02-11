fecee913da1605c3ade838fe67e029079eaef243
Gustavo <gustavo.pereira@discente.ufg>
2025-02-11 20:19:48 -0300

    feat: validação dos campos

--- src/main/java/com/gustavohenrique/codiNomeUol/controller/forFront/CadastroJogadorController.java
+++ src/main/java/com/gustavohenrique/codiNomeUol/controller/forFront/CadastroJogadorController.java
@@ -3,8 +3,10 @@ package com.gustavohenrique.codiNomeUol.controller.forFront;
 import com.gustavohenrique.codiNomeUol.model.GrupoCodinome;
 import com.gustavohenrique.codiNomeUol.model.Jogador;
 import com.gustavohenrique.codiNomeUol.service.JogadorService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,18 +24,19 @@ public class CadastroJogadorController {
 
     @GetMapping
     public String paginaCadastroJogador(Model model){
-        model.addAttribute("jogador", new Jogador(null,null,null,null,null));
-        model.addAttribute("grupo", GrupoCodinome.values());
-        return "cadastro-jogador";
+        return getViewAndModel(model, new Jogador(null,null,null,null,null));
     }
 
     @PostMapping
-    public String cadastrarJogador(@ModelAttribute Jogador jogador){
-        try {
-            var novoJogador = jogadorService.cadastrarJogador(jogador);
+    public String cadastrarJogador(@ModelAttribute @Valid Jogador jogador, BindingResult result, Model model) throws Exception {
+        if(result.hasErrors())return getViewAndModel(model,jogador);
+        var novoJogador = jogadorService.cadastrarJogador(jogador);
             return "redirect:cadastro-jogador";
-        } catch (Exception e) {
-            return "redirect:cadastro-jogador";
-        }
+    }
+
+    private String getViewAndModel(Model model, Jogador jogador) {
+        model.addAttribute("jogador", jogador);
+        model.addAttribute("grupo", GrupoCodinome.values());
+        return "cadastro-jogador";
     }
 }
--- src/main/java/com/gustavohenrique/codiNomeUol/model/Jogador.java
+++ src/main/java/com/gustavohenrique/codiNomeUol/model/Jogador.java
@@ -1,11 +1,20 @@
 package com.gustavohenrique.codiNomeUol.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 public record Jogador(
+        @NotBlank
         String nome,
+        @NotBlank
+        @Email
         String email,
         String telefone,
         String codinome,
+        @NotNull
         GrupoCodinome grupo
 ) {
 }
